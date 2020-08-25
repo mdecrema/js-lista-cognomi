d447a6b1805f77ec754a3d531056815ee0e90429
marco <marcodecrema@libero.it>
2020-08-25 18:17:37 +0200

    restituisco la posizione dell'elemento nell'array

--- js/script.js
+++ js/script.js
@@ -4,10 +4,15 @@ var cognome = prompt("Inserisci il tuo cognome");
 var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 // Aggiungo il cognome inserito dall'utente alla lista di cognomi
 listaCognomi.push(cognome);
+// Metodo per ordinare in modo alfabetico gli elementi all'interno dell'array
 listaCognomi.sort();
 
+// Ciclo per poter stampare gli elementi all'interno della pagina
 for (i=0;i<listaCognomi.length;i++) {
   var contenuto = document.getElementById("listaOrdinata").innerHTML;
   contenuto += "<li>" + listaCognomi[i] + "</li>";
   document.getElementById("listaOrdinata").innerHTML=contenuto;
 }
+
+// Restituisco la posizione nell'array dell'elemento inserito dall'utente
+console.log(listaCognomi.indexOf(cognome));
