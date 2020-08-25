3c131a196904a879efe289e4e0b7fbe47dab9c19
marco <marcodecrema@libero.it>
2020-08-25 17:23:40 +0200

    lista ordinata cognomi

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 var cognome = prompt("Inserisci il tuo cognome");
 // Array contenente tutti i cognomi
 var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+// Aggiungo il cognome inserito dall'utente alla lista di cognomi
+listaCognomi.push(cognome);
+listaCognomi.sort();
+
+for (i=0;i<listaCognomi.length;i++) {
+  var contenuto = document.getElementById("listaOrdinata").innerHTML;
+  contenuto += "<li>" + listaCognomi[i] + "</li>";
+  document.getElementById("listaOrdinata").innerHTML=contenuto;
+}
